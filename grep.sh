981400b9e0b4959bfc5708ad9e4660eb77c1731a
Jakub Muszynski <jakubmuszynski2103@gmail.com>
2024-08-14 10:50:05 +0200

    chore: setup for directory restructure and code refactor

--- grep.sh
+++ grep.sh
@@ -4,12 +4,12 @@
 
 set -e # Exit early if any commands fail
 
-# - Edit this to change how your program compiles locally
+# Compile the project
 (
   cd "$(dirname "$0")" # Ensure compile steps are run within the repository directory
   cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
-  cmake --build ./build
+  cmake --build build
 )
 
-# - Edit this to change how your program runs locally
-exec ./build/exe "$@"
+# Run the executable
+exec ./build/bin/grep_exe "$@"
